15b5c8336f7defe83d5f2fafefcee137c60ed69d
Felix Lägeler <felixlaegeler@gmail.com>
2014-10-24 17:20:54 +0200

    adapted readme

--- src/main/scala/de/htwg/modprog/cubic/impl/CubicBoard.scala
+++ src/main/scala/de/htwg/modprog/cubic/impl/CubicBoard.scala
@@ -7,5 +7,6 @@ class CubicBoard(cube: Vector[Int]) {
   def field(x: Int, y: Int, z: Int) = cube(coordToIndex(x, y, z))
   def fieldIsEmpty(x: Int, y: Int, z: Int) = field(x, y, z) == 0
   def insertCoin(x: Int, y: Int, z: Int, value: Int) = new CubicBoard(cube.updated(coordToIndex(x, y, z), value))
+  def hasWinner = ???
   override def toString() = "board: " + sideLength + ("x" + sideLength) * 2 + ", content: " + cube
 }
