40332f0b08362ba43b303307cf6019b73474bc6a
Felix Lägeler <felixlaegeler@gmail.com>
2014-11-02 17:46:01 +0100

    small changes

---
+++ src/main/scala/de/htwg/modprog/cubic/model/Result.scala
@@ -0,0 +1,3 @@
+package de.htwg.modprog.cubic.model
+
+class Result(player: Int, filledLine: Seq[(Int, Int, Int)])
--- src/main/scala/de/htwg/modprog/cubic/model/impl/CubicBoard.scala
+++ src/main/scala/de/htwg/modprog/cubic/model/impl/CubicBoard.scala
@@ -2,27 +2,40 @@ package de.htwg.modprog.cubic.model.impl
 
 import scala.annotation.tailrec
 import de.htwg.modprog.cubic.model.Board
+import de.htwg.modprog.cubic.model.impl.CubicBoard._
 
-class CubicBoard private(val cube: Vector[Int], val winningCoords: Vector[(Int, Int, Int)]) extends Board {
+class CubicBoard private(val cube: Vector[Int], val winningLines: Vector[Int]) extends Board {
   val n = math.cbrt(cube.length).toInt
-  val winningLines = for((x, y, z) <- winningCoords) yield (coordToIndex(x, y, z))
-  def coordToIndex(x: Int, y: Int, z: Int) = x + y * n + z * math.pow(n, 2).toInt
+  def coordToIndex = transformCoords(n) _
   override def field(x: Int, y: Int, z: Int) = cube(coordToIndex(x, y, z))
   override def fieldIsEmpty(x: Int, y: Int, z: Int) = field(x, y, z) == 0
   override def toString() = "board: " + n + ("x" + n) * 2 + ", content: " + cube
   override def insertCoin(x: Int, y: Int, z: Int, value: Int) = {
-    new CubicBoard(cube.updated(coordToIndex(x, y, z), value), winningCoords)
+    new CubicBoard(cube.updated(coordToIndex(x, y, z), value), winningLines)
   }
-  override def hasWinner = ???
+  override def hasWinner = {
+    ???
+  }
   def lineComplete(line: Vector[Int]) = line.forall(cube(_) == 1) || line.forall(cube(_) == 2)
 }
 
 object CubicBoard {
+  def transformCoords(n: Int)(x: Int, y: Int, z: Int) = x + y * n + z * math.pow(n, 2).toInt
   def apply(sideLength: Int) = {
     require(sideLength > 1)
     val cube = Vector.fill(math.pow(sideLength, 3).toInt)(0)
     val winningCoords = determineWinningCoords(sideLength)
     new CubicBoard(cube, winningCoords)
+  }
+  def spanLines(n: Int, base: Seq[(Int, Int, Int)], to: (Int, Int, Int)) = {
+    for ((x, y, z) <- base; i <- (0 until n)) yield (x + to._1  * i, y + to._2 * i, z + to._3 * i)
+  }
+  @tailrec
+  def span(n: Int, baseLines: Seq[(Int, Int, Int)], dirs: List[(Int, Int, Int)]): Seq[(Int, Int, Int)] = {
+    dirs match {
+      case Nil => baseLines
+      case d :: tail => span(n, spanLines(n, baseLines, d), tail)
+    }	  
   }
   def determineWinningCoords(n: Int) = {
     val max = n - 1
@@ -41,21 +54,7 @@ object CubicBoard {
         ((max,0,max), List((-1,1,-1))),					// ... #3
         ((max,0,0), List((-1,1,1)))						// ... #4
     )
-    val result = for((base, trans) <- winningLinesDescription) yield span(n, Vector(base), trans)
-    result.flatten
-  }
-  def spanLines(n: Int, base: Seq[(Int, Int, Int)], direction: (Int, Int, Int)) = {
-    val (vX, vY, vZ) = direction
-    for {
-      (x, y, z) <- base 
-      i <- (0 until n)
-    } yield (x + vX * i, y + vY * i, z + vZ * i)
-  }
-  @tailrec
-  def span(n: Int, baseLines: Seq[(Int, Int, Int)], dirs: List[(Int, Int, Int)]): Seq[(Int, Int, Int)] = {
-    dirs match {
-      case Nil => baseLines
-      case d :: tail => span(n, spanLines(n, baseLines, d), tail)
-    }	  
+    val result = (for((base, trans) <- winningLinesDescription) yield span(n, Vector(base), trans)).flatten
+    for((x, y, z) <- result) yield(transformCoords(n)(x, y, z))
   }
 }
