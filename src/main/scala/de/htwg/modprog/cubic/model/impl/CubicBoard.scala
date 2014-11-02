eccb2de21eaef147c8f91ed18f09eb6028479784
Marco <marco.grupe@googlemail.com>
2014-11-02 16:37:33 +0100

    add TUI

--- src/main/scala/de/htwg/modprog/cubic/model/impl/CubicBoard.scala
+++ src/main/scala/de/htwg/modprog/cubic/model/impl/CubicBoard.scala
@@ -3,9 +3,9 @@ package de.htwg.modprog.cubic.model.impl
 import scala.annotation.tailrec
 import de.htwg.modprog.cubic.model.Board
 
-class CubicBoard private(val cube: Vector[Int], val winningCoords: Vector[(Int, Int, Int)]) extends Board {
+class CubicBoard private (val cube: Vector[Int], val winningCoords: Vector[(Int, Int, Int)]) extends Board {
   val n = math.cbrt(cube.length).toInt
-  val winningLines = for((x, y, z) <- winningCoords) yield (coordToIndex(x, y, z))
+  val winningLines = for ((x, y, z) <- winningCoords) yield (coordToIndex(x, y, z))
   def coordToIndex(x: Int, y: Int, z: Int) = x + y * n + z * math.pow(n, 2).toInt
   override def field(x: Int, y: Int, z: Int) = cube(coordToIndex(x, y, z))
   override def fieldIsEmpty(x: Int, y: Int, z: Int) = field(x, y, z) == 0
@@ -15,6 +15,22 @@ class CubicBoard private(val cube: Vector[Int], val winningCoords: Vector[(Int, 
   }
   override def hasWinner = ???
   def lineComplete(line: Vector[Int]) = line.forall(cube(_) == 1) || line.forall(cube(_) == 2)
+
+  def createTUI = {
+    for (i <- 0 to 3) {
+      println(" " * (16 * i) + "----------------")
+      for (y <- (16 * i) until (16 * (i + 1))) {
+        if (y % 4 == 0) {
+          print(" " * (16 * i) + "|  " + cube.apply(y) + "  ")
+        } else if ((y - 3) % 4 == 0) {
+          println(cube.apply(y) + "  |")
+        } else {
+          print(cube.apply(y) + "  ")
+        }
+      }
+      println(" " * (16 * i) + "----------------")
+    }
+  }
 }
 
 object CubicBoard {
@@ -27,27 +43,27 @@ object CubicBoard {
   def determineWinningCoords(n: Int) = {
     val max = n - 1
     val winningLinesDescription = Vector(
-        ((0,0,0), List((0,1,0),(0,0,1),(1,0,0))), 		// lineset along x axis
-        ((0,0,0), List((1,0,0),(0,0,1),(0,1,0))), 		// ... y axis
-        ((0,0,0), List((1,0,0),(0,1,0),(0,0,1))), 		// ... z axis
-        ((0,0,0), List((1,0,0),(0,1,1))),				// diagonalset #1 along x axis
-        ((0,0,max), List((1,0,0),(0,1,1))),				// ... #2
-        ((0,0,0), List((0,1,0),(1,0,1))),				// diagonalset #1 along y axis
-        ((0,0,max), List((0,1,0),(1,0,-1))),			// ... #2
-        ((0,0,0), List((0,0,1),(1,1,0))),				// diagonalset #1 along z axis
-        ((max,0,0), List((0,0,1),(-1,1,0))),			// ... #2
-        ((0,0,0), List((1,1,1))),						// space diagonal #1
-        ((0,0,max), List((1,1,-1))),					// ... #2
-        ((max,0,max), List((-1,1,-1))),					// ... #3
-        ((max,0,0), List((-1,1,1)))						// ... #4
-    )
-    val result = for((base, trans) <- winningLinesDescription) yield span(n, Vector(base), trans)
+      ((0, 0, 0), List((0, 1, 0), (0, 0, 1), (1, 0, 0))), // lineset along x axis
+      ((0, 0, 0), List((1, 0, 0), (0, 0, 1), (0, 1, 0))), // ... y axis
+      ((0, 0, 0), List((1, 0, 0), (0, 1, 0), (0, 0, 1))), // ... z axis
+      ((0, 0, 0), List((1, 0, 0), (0, 1, 1))), // diagonalset #1 along x axis
+      ((0, 0, max), List((1, 0, 0), (0, 1, 1))), // ... #2
+      ((0, 0, 0), List((0, 1, 0), (1, 0, 1))), // diagonalset #1 along y axis
+      ((0, 0, max), List((0, 1, 0), (1, 0, -1))), // ... #2
+      ((0, 0, 0), List((0, 0, 1), (1, 1, 0))), // diagonalset #1 along z axis
+      ((max, 0, 0), List((0, 0, 1), (-1, 1, 0))), // ... #2
+      ((0, 0, 0), List((1, 1, 1))), // space diagonal #1
+      ((0, 0, max), List((1, 1, -1))), // ... #2
+      ((max, 0, max), List((-1, 1, -1))), // ... #3
+      ((max, 0, 0), List((-1, 1, 1))) // ... #4
+      )
+    val result = for ((base, trans) <- winningLinesDescription) yield span(n, Vector(base), trans)
     result.flatten
   }
   def spanLines(n: Int, base: Seq[(Int, Int, Int)], direction: (Int, Int, Int)) = {
     val (vX, vY, vZ) = direction
     for {
-      (x, y, z) <- base 
+      (x, y, z) <- base
       i <- (0 until n)
     } yield (x + vX * i, y + vY * i, z + vZ * i)
   }
@@ -56,6 +72,6 @@ object CubicBoard {
     dirs match {
       case Nil => baseLines
       case d :: tail => span(n, spanLines(n, baseLines, d), tail)
-    }	  
+    }
   }
 }
