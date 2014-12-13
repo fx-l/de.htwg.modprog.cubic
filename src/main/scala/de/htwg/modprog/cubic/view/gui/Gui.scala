42b3e5a844a3f121254c74e38a802fd750cddcde
Felix L <felixlaegeler@gmail.com>
2014-12-14 00:03:09 +0100

    continued with gui implementation

--- src/main/scala/de/htwg/modprog/cubic/view/gui/Gui.scala
+++ src/main/scala/de/htwg/modprog/cubic/view/gui/Gui.scala
@@ -7,69 +7,137 @@ import scalafx.event.{ Event, ActionEvent }
 import scalafx.scene.control.MenuItem._
 import scalafx.scene.control._
 import scalafx.scene.input.KeyCombination
-import scalafx.scene.layout.{ VBox, BorderPane }
+import scalafx.scene.layout._
 import scalafx.scene._
+import scalafx.scene.transform._
 import scalafx.beans.property.DoubleProperty
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.transform.Rotate
 import scalafx.scene.SceneAntialiasing
 import scalafx.scene.paint.{ Color, PhongMaterial }
-import scalafx.scene.shape.{ Sphere, Box }
+import scalafx.scene.shape._
 import scalafx.geometry.Insets
+import scalafx.beans.property.ReadOnlyDoubleProperty
+import scalafx.scene.text.Font
+import scalafx.scene.paint.{Stops, LinearGradient}
+import scalafx.scene.text.Text
+import scalafx.scene.effect.DropShadow
 
 object Gui extends JFXApp {
+  
+  val gameSize = 4
+  val sphereSize = 3
+  val spacer = 12
+  val sideLength = gameSize * sphereSize + (gameSize - 1) * spacer
+  val zoomOutFactor = 3
+  val horizontalCorrectionFactor = 0.08F;
 
-  val sphereSize = 50
-  val sphereMargin = 200
-
-  stage = new PrimaryStage {
+  stage = new PrimaryStage {     
     title = "Cubic"
     scene = new Scene(900, 900, true, SceneAntialiasing.Balanced) {
+      
+      var tmpw = this. width
+      var tmph =  this. height
+      
+      fill = Color.Black
+       
       root = new BorderPane {
-        top = createMenu
-        center = createGameWindow
-        bottom = createStatusBar
+        fill = Color.Black
+        top = new VBox {
+          content = Seq(createMenu, createStatusBar)
+        }
+        center = createView(tmpw,  tmph)
+      } 
+    }
+    width onChange show
+    height onChange show
+  }
+  
+  // creategame window
+  def createView(boundWidth: ReadOnlyDoubleProperty, boundHeight: ReadOnlyDoubleProperty): BorderPane = {
+    new BorderPane {
+      center = new SubScene(boundWidth.get(), boundHeight.get(), true, SceneAntialiasing.Balanced) {
+      fill = Color.Black
+  
+      val spheres = new Group
+      spheres.children = createSpheres(gameSize, sphereSize, spacer)
+      content = spheres
+         
+      camera = new PerspectiveCamera(true) {
+        farClip = sideLength + zoomOutFactor * sideLength
+        transforms += (
+            new Translate(0, sideLength * horizontalCorrectionFactor, -sideLength * zoomOutFactor))
       }
-      val shapes = new Group
-      shapes.children = createSpheres
-
-      val light = new PointLight {
-        color = Color.AntiqueWhite
-        translateX = -265
-        translateY = -260
-        translateZ = -625
+      this.width.bind(boundWidth.add(0))
+      this.height.bind(boundHeight.add(0))
+      addMouseInteraction(this, spheres)
       }
-      //      root = new Group {
-      //        // Put light outside of `shapes` group so it does not rotate
-      //        content = new Group(shapes, light)
-      //        translateX = 450
-      //        translateY = 250
-      //        translateZ = 825
-      //        rotationAxis = Rotate.YAxis
-      //      }
-
-      camera = new PerspectiveCamera(false)
-
-      addMouseInteraction(this, shapes)
+    }
+}
+  
+  // a method for creating the status bar
+  def createStatusBar = {
+    new VBox {
+          padding = Insets(8)
+          content = new Text {
+            text = "Status messages go here"
+            style = "-fx-font-size: 20pt"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(Color.Cyan, Color.DodgerBlue)
+            )
+            effect = new DropShadow {
+              color = Color.DodgerBlue
+              radius = 25
+              spread = 0.25
+            }
+          }
     }
   }
   
+  def createSpheres(gameSize: Int, sphereSize: Int, spacer: Int) = {
+    def coord(value: Int) = value * spacer - (gameSize - 1) * spacer / 2
+    for (x <- 0 until gameSize; y <- 0 until gameSize; z <- 0 until gameSize)
+      yield (
+      new Sphere(sphereSize) {
+        material = new PhongMaterial(Color.Red)
+        drawMode = DrawMode.Fill
+        translateX = coord(x)
+        translateY = coord(y)
+        translateZ = coord(z)
+        id = x + "-" + y + "-" + z
+      })
+  }
   
-  /** Add mouse interaction to a scene, rotating given node. */
-  private def addMouseInteraction(scene: Scene, group: Group) {
-    val angleY = DoubleProperty(-50)
+    def addMouseInteraction(scene: SubScene, group: Group) {
+    
+    val angleX = DoubleProperty(0)
+    val angleY = DoubleProperty(0)
+    
+    val xRotate = new Rotate {
+      angle <== angleX
+      axis = Rotate.XAxis
+    }
+    
     val yRotate = new Rotate {
       angle <== angleY
       axis = Rotate.YAxis
     }
+    
     var anchorX: Double = 0
+    var anchorY: Double = 0
+    
+    var anchorAngleX: Double = 0
     var anchorAngleY: Double = 0
 
-    group.transforms = Seq(yRotate)
+    group.transforms = Seq(xRotate, yRotate)
 
     scene.onMousePressed = (event: MouseEvent) => {
+      anchorAngleX = angleX()
       anchorAngleY = angleY()
+      
       anchorX = event.sceneX
+      anchorY = -event.sceneY
 
       // Retrieve information about a pick
       val pickResult = event.pickResult
@@ -86,59 +154,21 @@ object Gui extends JFXApp {
     }
 
     scene.onMouseDragged = (event: MouseEvent) => {
+      angleX() = anchorAngleX + anchorY - (-event.sceneY)
       angleY() = anchorAngleY + anchorX - event.sceneX
     }
-
   }
-
-  private def createMarker(x: Double, y: Double, z: Double): Sphere = new Sphere(35) {
-    material = new PhongMaterial {
-      diffuseColor = Color.Gold
-      specularColor = Color.LightGreen
-    }
+    
+  def createMarker(x: Double, y: Double, z: Double) : Sphere = new Sphere(1) {
+    material = new PhongMaterial(Color.Gold)
     translateX = x
     translateY = y
     translateZ = z
   }
-
-  def createSpheres = {
-    for (x <- 0 until 4; y <- 0 until 4; z <- 0 until 4)
-      yield (
-      new Sphere(sphereSize) {
-        material = new PhongMaterial {
-          diffuseColor = Color.Red
-          specularColor = Color.Pink
-        }
-        translateX = x * sphereMargin
-        translateY = y * sphereMargin
-        translateZ = z * sphereMargin
-        id = x + "-" + y + "-" + z
-      })
-  }
-  
-  // creategame window
-  def createGameWindow = {
-    new VBox {
-          content = new Label {
-            text = "Game will be here"
-          }
-    }
-  }
-  
-  // a method for creating the status bar
-  def createStatusBar = {
-    new VBox {
-          padding = Insets(14)
-          content = new Label {
-            text = "Status Messages go here"
-            id = "statusLabel"
-          }
-    }
-  }
   
   // a method for building the menu
   def createMenu = new VBox {
-          content = List(new MenuBar {
+    content = List(new MenuBar {
     menus = List(
       new Menu("Game") {
         items = List(new Menu("New Game") {
@@ -184,10 +214,9 @@ object Gui extends JFXApp {
             onAction = {
               e: ActionEvent => println(e.eventType + " About... clicked!")
             }
-          }
-          )
-      }  
-    )
-  })
-        }
+          })
+      })
+    })
+  }
+    
 }
